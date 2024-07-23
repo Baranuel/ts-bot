27771b13915048e6f3307a57375399f84ca1e5e6
Samuel Baran <s.baran.dev@gmail.com>
2024-07-23 23:02:19 +0200

    accept cookies, reload after ticket found

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 import puppeteer from "puppeteer-extra";
 import plugin from "puppeteer-extra-plugin-stealth";
 import sound from "sound-play";
+import UserAgent from "user-agents";
 
 import path from "path";
 
 // Or import puppeteer from 'puppeteer-core';
 
-const StealthPlugin = plugin();
+const StealthPlugin = plugin({
+  enabledEvasions: [
+    "chrome.runtime",
+    "navigator.webdriver",
+    "navigator.languages",
+    "navigator.permissions",
+    "navigator.plugins",
+  ],
+});
 puppeteer.use(StealthPlugin);
+console.log(StealthPlugin.enabledEvasions);
 
 const fanSale =
-  "https://www.fansale.at/tickets/all/taylor-swift/502069/17335909";
+  "https://www.fansale.at/tickets/all/taylor-swift/502069/17275983";
 const REFRESH_INTERVAL = 5000;
-const pathToHorn = path.join(import.meta.dirname, "horn.mp3");
+const pathToHorn = path.join(import.meta.dirname, "notify.mp3");
 const browser = await puppeteer.launch({
   channel: "chrome",
   headless: false,
@@ -25,43 +35,76 @@ const scrollElementToView = async (element) => {
   await page.evaluate((element) => element.scrollIntoView(), element);
 };
 
+const reloadPageAfterSelection = async () => {
+  sound.play(pathToHorn);
+  await page.reload();
+};
+
+async function acceptCookies() {
+  const accessAllowed = await page.$(".DefaultPage");
+  if (!accessAllowed) return;
+
+  const cookiesButton = (
+    await page.evaluateHandle(
+      'document.querySelector("#cmpwrapper").shadowRoot.querySelector("#cmpwelcomebtnyes > a")'
+    )
+  ).asElement();
+  if (!cookiesButton) return;
+  try {
+    await cookiesButton.click();
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+}
+
+const clickVisibleTicket = async (ticket) => {
+  const isVisible = await ticket.isVisible();
+  const isIntersecting = await ticket.isIntersectingViewport();
+  console.log("clicking the ticket", ticket);
+  isVisible && isIntersecting && (await ticket.click());
+};
+
 const selectTicket = async () => {
-  sound.play(pathToHorn); // play horn when ticket was found
   const ticket = await page.$(".EventEntry");
   const ticketIcon = await page.$(".TicketcheckIcon");
-  console.log("Ticket: ", ticket);
-  console.log("TicketIcon: ", ticketIcon);
   const numberOfTickets = await page.$(".NumberOfTicketsInOffer");
 
   if (!ticket && !ticketIcon) {
     console.log("No tickets can be selected");
-    return setTimeout(reload, 5000);
+    return setTimeout(reload, REFRESH_INTERVAL);
   }
   const numberOfTicketsValue = await page.evaluate(
     (ticket) => ticket.textContent,
     numberOfTickets
   );
   console.log("Number of tickets available: ", numberOfTicketsValue);
+
   try {
-    await ticket.click();
+    await clickVisibleTicket(ticket);
+    await reloadPageAfterSelection();
+    await clickVisibleTicket(ticket);
+
     await page.waitForNavigation();
     await page.click("ZUR KASSE");
   } catch (error) {
-    await ticketIcon.click();
-    await page.waitForNavigation();
-    await page.click("ZUR KASSE");
+    console.log("Error: ", error);
   }
 };
 
 let element = null;
 
 const reload = async () => {
-  console.log("Looking for tickets")
+  const randomUserAgent = new UserAgent({ deviceCategory: "mobile" })
+    .random()
+    .toString();
+  await page.setUserAgent(randomUserAgent);
+  console.log("Looking for tickets as ", randomUserAgent);
   try {
     await page.reload();
+    await acceptCookies();
     const ticketDiv = await page.$(".EventEntryList");
     element = ticketDiv; // place where the tickets are shown
-    
+
     if (!element) return setTimeout(reload, REFRESH_INTERVAL); // no Tickets
 
     await scrollElementToView(element);
@@ -72,3 +115,4 @@ const reload = async () => {
   }
 };
 reload();
+sound.play(pathToHorn);
